afacd71c30468850bb8ce52c6533f35c79f5d438
Abdus Shakil Chowdhury <a.shakil@outlook.com>
2024-06-15 01:55:55 +0600

    More Functionality Added

--- Lilabali/Controllers/LilabaliController.cs
+++ Lilabali/Controllers/LilabaliController.cs
@@ -1,4 +1,5 @@
 ﻿using Lilabali.Models;
+using Lilabali.Repo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +14,43 @@ namespace Lilabali.Controllers
         public LilabaliDbContext lbc = new LilabaliDbContext();
         public ActionResult Home()
         {
+            DropDownRepo ddr = new DropDownRepo();
+            var team = lbc.Teams.ToList();
+            if (team.Count > 0)
+            {
+                ViewBag.Teams = ddr.GetTeam();
+            }
+            else
+            {
+                ViewBag.Teams = null;
+            }
             return View();
         }
         [HttpPost]//Members will be added through this method
         public ActionResult AddMember(Member M)
         {
-            if (M != null)
+            if (M.TID > 0)
             {
-
+                lbc.Members.Add(M);
+                lbc.SaveChanges();
             }
             return RedirectToAction("Home");
+        }
+        public ActionResult Teams()
+        {
+            return View();
+        }
+        public ActionResult AddTeam(string Tname)
+        {
+            Team tms = new Team();
+            if (!string.IsNullOrEmpty(Tname))
+            {
+                tms.TeamName = Tname;
+                tms.IsActive = 1;
+                lbc.Teams.Add(tms);
+                lbc.SaveChanges();
+            }
+            return RedirectToAction("Teams");
         }
     }
 }
---
+++ Lilabali/Repo/DropDownRepo.cs
@@ -0,0 +1,23 @@
+﻿using Lilabali.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lilabali.Repo
+{
+    public class DropDownRepo
+    {
+        public LilabaliDbContext ldb = new LilabaliDbContext();
+        public IEnumerable<SelectListItem> GetTeam()
+        {
+            IEnumerable<SelectListItem> datas = ldb.Teams.Select(n => new SelectListItem
+            {
+                Value = n.TID.ToString(),
+                Text = n.TeamName,
+            }).Distinct().ToList();
+            return new SelectList(datas, "Value", "Text");
+        }
+    }
+}
