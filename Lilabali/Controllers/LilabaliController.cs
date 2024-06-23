769a14374d9bc931e118947c63e31f36212aed73
Md Mahabubul Alam Abir <mahbubabir09@gmail.com>
2024-06-23 10:59:35 +0600

    Toast Updated

--- Lilabali/Controllers/LilabaliController.cs
+++ Lilabali/Controllers/LilabaliController.cs
@@ -34,17 +34,21 @@ namespace Lilabali.Controllers
         public ActionResult AddMember(Member M)
         {
             string msg;
+            string msgType;
             if (M.TID > 0)
             {
                 lbc.Members.Add(M);
                 lbc.SaveChanges();
                 msg = "Saved Successfully";
+                msgType = "success";
             }
             else
             {
                 msg = "Not Saved";
+                msgType = "error";
             }
             TempData["message"] = msg;
+            TempData["messageType"] = msgType;
             return RedirectToAction("Home");
         }
         [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
@@ -58,6 +62,7 @@ namespace Lilabali.Controllers
         {
             Team tms = new Team();
             string msg;
+            string msgType;
             if (!string.IsNullOrEmpty(Tname))
             {
                 tms.TeamName = Tname;
@@ -65,12 +70,15 @@ namespace Lilabali.Controllers
                 lbc.Teams.Add(tms);
                 lbc.SaveChanges();
                 msg = "Saved Successfully";
+                msgType = "success";
             }
             else
             {
                 msg = "Not Saved";
+                msgType = "error";
             }
             TempData["message"] = msg;
+            TempData["messageType"] = msgType;
             return RedirectToAction("Teams");
         }
         [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
@@ -85,9 +93,10 @@ namespace Lilabali.Controllers
         public ActionResult CreateBill(PaymentVM pvm)
         {
             string msg;
+            string msgType;
             DateTime date = DateTime.Now.Date;
             datewisePayment dt = new datewisePayment();
-            if(pvm.SelectedMembers.Count>0|| pvm.SelectedMembers != null)
+            if (pvm.SelectedMembers.Count > 0 || pvm.SelectedMembers != null)
             {
                 foreach (var item in pvm.SelectedMembers)
                 {
@@ -95,21 +104,20 @@ namespace Lilabali.Controllers
                     dt.HostID = pvm.Host;
                     dt.MID = Convert.ToInt32(item);
                     dt.Amount = pvm.Amount / pvm.SelectedMembers.Count();
-                    if (dt.MID == pvm.Host)
-                    {
-                        dt.P_Status = 1;
-                    }
-                    else { dt.P_Status = 0; };
+                    dt.P_Status = (dt.MID == pvm.Host) ? 1 : 0;
                     lbc.datewisePayments.Add(dt);
                     lbc.SaveChanges();
                 }
                 msg = "Bill has been Created Successfully";
+                msgType = "success";
             }
             else
             {
                 msg = "Something Went Wrong";
+                msgType = "error";
             }
             TempData["message"] = msg;
+            TempData["messageType"] = msgType;
             return RedirectToAction("Payment");
         }
         [HttpPost]
