382e7c96dfd37064f2a15713eab641f99b3349d3
Abdus Shakil Chowdhury <a.shakil@outlook.com>
2024-06-25 23:35:11 +0600

    Reports Module

---
+++ Lilabali/ViewModels/BillReportView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lilabali.ViewModels
+{
+    public class BillReportView
+    {
+        public DateTime P_Date { get; set; }
+
+        public int MID { get; set; }
+
+        public string M_Name { get; set; }
+
+        public int HostID { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public int P_Status { get; set; }
+    }
+}
